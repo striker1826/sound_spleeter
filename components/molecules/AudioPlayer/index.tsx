6fbac9afecec6eaac274555ad1943019263c0ff7
striker1826 <alstjq123579@daum.net>
2025-03-22 00:52:23 +0900

    fix: 모바일 기기에서 재생바 조작 시 싱크 안맞는 문제

--- components/molecules/AudioPlayer/index.tsx
+++ components/molecules/AudioPlayer/index.tsx
@@ -180,24 +180,15 @@ const AudioPlayer: React.FC<AudioPlayerProps> = ({
     }
   }, [isPlaying]);
 
-  // 시간 변경 시 seek 호출
+  // currentTime 변경 시 오디오 재생 위치 업데이트
   useEffect(() => {
-    if (soundRef.current) {
-      const currentSeek = soundRef.current.seek();
-      if (Math.abs(currentSeek - currentTime) > 0.1) {
-        soundRef.current.seek(currentTime);
-      }
+    if (!soundRef.current) return;
+
+    const currentSeek = soundRef.current.seek() as number;
+    if (Math.abs(currentSeek - currentTime) > 0.1) {
+      soundRef.current.seek(currentTime);
     }
   }, [currentTime]);
-
-  useEffect(() => {
-    return () => {
-      if (soundRef.current) {
-        soundRef.current.stop();
-        soundRef.current.unload();
-      }
-    };
-  }, []);
 
   const togglePlay = () => {
     if (!soundRef.current) return;
